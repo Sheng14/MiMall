dc3afc4ac8205e6499b3e1a909c471630e737e03
Sheng14 <1336840941@qq.com>
2020-10-18 16:24:04 +0800

    feat: 目录结构设置

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view><router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
  import axios from 'axios'
 // import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   },
   data () {
     return {
@@ -27,14 +26,15 @@ export default {
   }*/
   
   mounted () {
-   // let url = 'https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list'
-   let url1 = '/api/learningpath/relatedpath'
+  
+  // let url = 'https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list'
+  // let url1 = '/api/learningpath/relatedpath'
     // 真实路径：https://coding.imooc.com/learningpath/relatedpath 转发路径： http://localhost:8080/api/learningpath/relatedpath
-   let url2 = '/api/index/getadver' 
+  // let url2 = '/api/index/getadver' 
    // 真实路径： https://coding.imooc.com/index/getadver 转发路径： http://localhost:8080/api/index/getadver
-   let url3 = '/api/class/ajaxprelearn'
+  // let url3 = '/api/class/ajaxprelearn'
     // 真实路径： https://coding.imooc.com/class/ajaxprelearn 转发路径： http://localhost:8080/api/class/ajaxprelearn
-   axios.get(url1).then((info) => {
+  /* axios.get(url1).then((info) => {
       console.log(info)
    })
    axios.get(url2).then((info) => {
@@ -42,7 +42,7 @@ export default {
    })
    axios.get(url3).then((info) => {
       console.log(info)
-   })
+   })*/
   }
 }
 </script>
