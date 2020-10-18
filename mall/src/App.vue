966d165b2b7ffc365229d1b99cca52255d65cabf
Sheng14 <1336840941@qq.com>
2020-10-18 15:09:41 +0800

    feat: 学习接口代理

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -7,8 +7,8 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-// import axios from 'axios'
-import jsonp from 'jsonp'
+ import axios from 'axios'
+// import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
@@ -19,19 +19,31 @@ export default {
       age: 30
     }
   },
-  mounted () {
+ /* mounted () {
     let url = 'https://www.imooc.com/search/hotwords'
     jsonp(url, (err, res) => {
       console.log(res)
     })
-  }
-  /*
-  mounted () {
-    let url = 'https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list'
-    axios.get(url).then((info) => {
-      console.log(info)
-    })
   }*/
+  
+  mounted () {
+   // let url = 'https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list'
+   let url1 = '/api/learningpath/relatedpath'
+    // 真实路径：https://coding.imooc.com/learningpath/relatedpath 转发路径： http://localhost:8080/api/learningpath/relatedpath
+   let url2 = '/api/index/getadver' 
+   // 真实路径： https://coding.imooc.com/index/getadver 转发路径： http://localhost:8080/api/index/getadver
+   let url3 = '/api/class/ajaxprelearn'
+    // 真实路径： https://coding.imooc.com/class/ajaxprelearn 转发路径： http://localhost:8080/api/class/ajaxprelearn
+   axios.get(url1).then((info) => {
+      console.log(info)
+   })
+   axios.get(url2).then((info) => {
+      console.log(info)
+   })
+   axios.get(url3).then((info) => {
+      console.log(info)
+   })
+  }
 }
 </script>
 
