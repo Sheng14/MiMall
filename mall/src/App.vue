e1e3e7c52f1fbe7a5f96dff6f754a339c8f2d203
Sheng14 <1336840941@qq.com>
2020-10-18 14:03:25 +0800

    feat: 学习jsonp

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -7,7 +7,8 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+// import axios from 'axios'
+import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
@@ -17,7 +18,20 @@ export default {
     return {
       age: 30
     }
+  },
+  mounted () {
+    let url = 'https://www.imooc.com/search/hotwords'
+    jsonp(url, (err, res) => {
+      console.log(res)
+    })
   }
+  /*
+  mounted () {
+    let url = 'https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list'
+    axios.get(url).then((info) => {
+      console.log(info)
+    })
+  }*/
 }
 </script>
 
