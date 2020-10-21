5c57906e038e856c3eb7f3e7d744a75541256dac
Sheng14 <1336840941@qq.com>
2020-10-21 19:40:22 +0800

    feat: 接口环境设置

---
+++ mall/src/env.js
@@ -0,0 +1,20 @@
+let baseURL = ''
+
+switch (process.env.NODE_ENV) { // 根据环境变量选择不同的url地址
+    case 'development':
+        baseURL = 'http://dev-mall-pre.springboot.cn/api';
+        break;
+    case 'test':
+        baseURL = 'http://test-mall-pre.springboot.cn/api';
+        break;
+    case 'production':
+        baseURL = 'http://mall-pre.springboot.cn/api';
+        break;
+    default:
+        baseURL = 'http://mall-pre.springboot.cn/api';
+        break;
+}
+
+export default {
+    baseURL
+}
--- mall/src/main.js
+++ mall/src/main.js
@@ -3,13 +3,17 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import router from './router'
+import env from './env'
 
 // axios的默认配置，需要根据前端的跨域方式进行调整
 axios.defaults.baseURL = '/api'
 axios.defaults.timeout = 8000
 
+// 根据环境变量的不同获取不同的请求地址
+axios.defaults.baseURL = env.baseURL
+
 // 接口错误拦截
-axios.interceptors.response(function(response){
+axios.interceptors.response.use(function(response){
   let res = response.data
   if(res.status == 0) { // 成功状态
     return res.data
