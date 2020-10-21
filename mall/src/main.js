42a8785095aba915ca63de7860bd9cd6da161acb
Sheng14 <1336840941@qq.com>
2020-10-21 15:59:56 +0800

    feat: 接口错误拦截

--- mall/src/main.js
+++ mall/src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
 
+// axios的默认配置，需要根据前端的跨域方式进行调整
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+
+// 接口错误拦截
+axios.interceptors.response(function(response){
+  let res = response.data
+  if(res.status == 0) { // 成功状态
+    return res.data
+  } else if (res.status === 10) { // 未登录状态
+    window.location.href = '/#/login'
+  } else { // 错误状态
+    alert(res.msg)
+  }
+})
+
+Vue.use(axios, VueAxios)
 Vue.config.productionTip = false
 
 new Vue({
