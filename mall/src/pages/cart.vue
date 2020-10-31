4756f093dff580fd1028db1efe6405460a222c52
Sheng14 <1336840941@qq.com>
2020-10-31 10:26:29 +0800

    feat: 10-1 Order-header组件

--- mall/src/pages/cart.vue
+++ mall/src/pages/cart.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-        <order-header></order-header>
-        <div>cart</div>
+        <order-header title="购物车页面">
+            <template v-slot:tip>
+                <span>温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算</span>
+            </template>
+        </order-header>
         <nav-footer></nav-footer>
     </div>
 </template>
