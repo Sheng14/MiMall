a8ffe52834f6cea015b53c102742c8af8fcf593a
Sheng14 <1336840941@qq.com>
2020-11-01 12:47:07 +0800

    feat: 12-1订单父组件结构封装

--- mall/src/pages/order.vue
+++ mall/src/pages/order.vue
@@ -1,18 +1,44 @@
 <template>
     <div>
-        <order-header></order-header>
+        <order-header :title="title">
+            <template v-slot:tip>
+                <span>{{tip}}</span>
+            </template>
+        </order-header>
         <router-view></router-view>
+        <search-bar></search-bar>
         <nav-footer></nav-footer>
     </div>
 </template>
 <script>
 import NavFooter from '../components/NavFooter'
 import OrderHeader from '../components/OrderHeader'
+import SearchBar from '../components/SearchBar'
 export default {
     name: 'order',
     components: {
         NavFooter,
-        OrderHeader
+        OrderHeader,
+        SearchBar
+    },
+    data () {
+        return {
+            title: '',
+            tip: ''
+        }
+    },
+    mounted () {
+        let path = this.$route.path // 获取当前页面路由
+        if (path === '/order/confirm') { // 根据不同路由给予不同的title和tip
+            this.title = '订单确认'
+            this.tip = '请认真填写收货地址'
+        } else if (path === '/order/list') {
+            this.title = '订单列表'
+            this.tip = '请谨防钓鱼链接或诈骗电话，了解更多>'
+        } else if (path === '/order/pay') {
+            this.title = '订单支付'
+            this.tip = '请谨防钓鱼链接或诈骗电话，了解更多>'
+        }
     }
 }
 </script>
