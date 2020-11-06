ba27a81fb2b9cecae6a80c5ff7a0b4864429552f
Sheng14 <1336840941@qq.com>
2020-11-06 14:29:13 +0800

    feat: vue。config.js配置介绍

--- mall/vue.config.js
+++ mall/vue.config.js
@@ -11,5 +11,10 @@ module.exports = {
                 }
             }
         }
-    }
+    },
+    outputDir: 'ODST', // 打包输出文件夹名称
+    publicPath: '/app', // 配置二级目录，域名下的下一个目录
+    indexPath: 'index2.html', // 入口文件路径/名称
+    lintOnSave: false, // 开启eslint否
+    productionSourceMap: false // 开启map文件与否
 }
