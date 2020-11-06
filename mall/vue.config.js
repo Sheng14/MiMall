a9b2b30aa25676af3320f235d334e024e25c4dc4
Sheng14 <1336840941@qq.com>
2020-11-06 17:07:06 +0800

    feat: 15-5删除预加载-真正按需

--- mall/vue.config.js
+++ mall/vue.config.js
@@ -12,5 +12,8 @@ module.exports = {
             }
         }
     },
-    productionSourceMap: false
+    productionSourceMap: false,
+    chainWebpack:(config)=>{
+        config.plugins.delete('prefetch');
+    }
 }
